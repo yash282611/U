6a1d9fda30379cf22d9aba0af88d6a2844c34db9
yash282611 <yash827830@gmail.com>
2026-08-28 23:30:31 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", "33707541"))
+API_HASH = os.environ.get("API_HASH", "5fc354872ad83bf6138b998095a69401")
+SESSION_STRING = os.environ.get("SESSION_STRING", "BQICVhUAsn7XKUQn-ipqlueMpP7Q_89eJ6Wa87FvrEtdIW2hI4n9TYo427D9Zb5WMxf2bq2CkR5IrxtgKS1beAu8SY19zqHFW4EOQdAk2xfOCYqfzBeDwocxo7nVBLJsZOet81ASp6DM0v6Bb4RtwgtMgdjYYcm22-w1ZzZc2M1Z8ReHXCpc4qG2XF7vf3-eZrVtF58cw7dawyH761k6hgrt_5UKMnJ3Su-Oi_o-OXtuoXmonZtaJtH5GZiYVJyo0PBPXBTEX5mvYce7nQRPgRB7-pcglSWDmEIhm6ZDm0xPHxvPuM5IHb7nMAaM3vf33ZkqaD7mn9naf3v8ykxDMZteAgoQAAAAHv9mA-AA")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+GROUP_LINK = os.environ.get("GROUP_LINK", "https://t.me/Period_Blocker")
