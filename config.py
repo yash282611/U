3025423ade95251b53791ea54cdf62624a029c41
yash282611 <yash827830@gmail.com>
2026-08-29 16:41:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ API_ID = int(os.environ.get("API_ID", "33707541"))
 API_HASH = os.environ.get("API_HASH", "5fc354872ad83bf6138b998095a69401")
 SESSION_STRING = os.environ.get("SESSION_STRING", "BQICVhUAEbPkcGB_kiBgbWSADdiYY6B2h0-5pl7Kr4sD-kOfq2Yk6QFPh-l7ryS3EvzH-ADpDJfYFZ94nmuLM84SLF_1-d1HhYH7SHwz62EslT4StOCB_rLmeq4HWTtpSisB4TFXAZ8OpZwCP0ER5LB04TK5gh880hmJVFnMxKyKLcTPd87pBg54mAxUkXPmeGogHkowKluzXp0iz90Ln7qVz-AIh-iF3OTixCt8HkFedBjnjlTYTXPW6TtIpdxYi-S1Sj4JNgfjqvIQ8y4D11RT3lEryFbqc4wVhrEahg2_2V9TmrBKCyqUCEuH7lbULnrAOKpm3oNMhmZ7APT02zZifOvIxAAAAAFmfq8wAA")
 
-# यहाँ अपनी असली GROK_API_KEY डाल देना
-GROK_API_KEY = os.environ.get("GROK_API_KEY")
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 
 GROUP_LINK = os.environ.get("GROUP_LINK", "https://t.me/Period_Blocker")
